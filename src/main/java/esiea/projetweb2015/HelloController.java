c1e4b247a5c387d9ca306f26ca6a92fc5df83949
Minipiix <incardona@et.esiea.fr>
2015-09-21 19:03:43 +0200

    Premier commit

---
+++ src/main/java/esiea/projetweb2015/HelloController.java
@@ -0,0 +1,14 @@
+package esiea.projetweb2015;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
