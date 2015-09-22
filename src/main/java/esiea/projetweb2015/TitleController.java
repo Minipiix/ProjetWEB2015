7b77d0b4a3c45c8fbc45dd80ed1183384085944c
Minipiix <incardona@et.esiea.fr>
2015-09-22 15:33:43 +0200

    Les titres et le corps se modifient en fonction des boutons

--- src/main/java/esiea/projetweb2015/TitleController.java
+++ src/main/java/esiea/projetweb2015/TitleController.java
@@ -2,22 +2,20 @@ package esiea.projetweb2015;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/hello-world")
-public class HelloWorldController {
-	
-	private static final String template = "Hello %s!";
+@RequestMapping("/title")
+public class TitleController {
+	private static final String template = "Toute l'actus de %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public @ResponseBody Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name){
+	public @ResponseBody Greeting changeTitle(@RequestParam(value="name", required=false, defaultValue="Blizzard") String name){
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
-	
 }
--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -1,26 +1,114 @@
 $(document).ready(
 	function(){
 		console.log("ready!");
-		$("#hello").click(hello);
 		$("#diabloButton").click(diablo);
 		$("#hearthstoneButton").click(hearthstone);
+		$("#starcraftButton").click(starcraft);
+		$("#wowButton").click(wow);
+		$("#herosOfTheStormButton").click(herosOfTheStorm);
 	}
 );
 
 function diablo(){
-		console.log("MDR DIABLO");
-	};
-	
-function hearthstone(){
-		console.log("MDR HEARTHSTONE");
-	};
-	
-function hello(){
+	console.log("MDR DIABLO");
 	$.ajax({
-		url:"hello-world?name=Toto",
+		url:"title?name=Diablo",
 		method:"GET",
 		success:function(data) {
-			$("#hello").html(data.content);
+			$("#titlePage").html(data.content);
 		}
 	});
+	$.ajax({
+		url:"hello-world?name=DiabloFan",
+		method:"GET",
+		success:function(data) {
+			$("#helloMessage").html(data.content);
+		}
+	});
+	$.ajax({
+		url:"diablo.html",
+		method:"GET",
+		success:function(data) {
+			$("#corpsSite").html(data);
+		}
+	});			
+};
+	
+function hearthstone(){
+	console.log("MDR HEARTHSTONE");
+	$.ajax({
+		url:"title?name=Hearthstone",
+		method:"GET",
+		success:function(data) {
+			$("#titlePage").html(data.content);
+		}
+	});
+	$.ajax({
+		url:"hello-world?name=HSFan",
+		method:"GET",
+		success:function(data) {
+			$("#helloMessage").html(data.content);
+		}
+	});
+	$.ajax({
+		url:"hearthstone.html",
+		method:"GET",
+		success:function(data) {
+			$("#corpsSite").html(data);
+		}
+	});	
+};
+
+function starcraft(){
+	console.log("MDR STARCRAFT");
+	$.ajax({
+		url:"title?name=Starcraft",
+		method:"GET",
+		success:function(data) {
+			$("#titlePage").html(data.content);
+		}
+	});
+	$.ajax({
+		url:"hello-world?name=SCFan",
+		method:"GET",
+		success:function(data) {
+			$("#helloMessage").html(data.content);
+		}
+	});		
+};
+
+function wow(){
+	console.log("MDR WOW");
+	$.ajax({
+		url:"title?name=World Of Warcraft",
+		method:"GET",
+		success:function(data) {
+			$("#titlePage").html(data.content);
+		}
+	});
+	$.ajax({
+		url:"hello-world?name=WowFan",
+		method:"GET",
+		success:function(data) {
+			$("#helloMessage").html(data.content);
+		}
+	});		
+};
+
+function herosOfTheStorm(){
+	console.log("MDR Heros Of The Storm");
+	$.ajax({
+		url:"title?name=Heros Of The Storm",
+		method:"GET",
+		success:function(data) {
+			$("#titlePage").html(data.content);
+		}
+	});
+	$.ajax({
+		url:"hello-world?name=HerosFan",
+		method:"GET",
+		success:function(data) {
+			$("#helloMessage").html(data.content);
+		}
+	});		
 };
