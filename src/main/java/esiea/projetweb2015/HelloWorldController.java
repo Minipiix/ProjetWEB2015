428e6580162efcbcdf478349caf997ecbbfd385a
Minipiix <incardona@et.esiea.fr>
2015-09-22 11:46:01 +0200

    test

--- src/main/java/esiea/projetweb2015/HelloWorldController.java
+++ src/main/java/esiea/projetweb2015/HelloWorldController.java
@@ -1,14 +1,23 @@
 package esiea.projetweb2015;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-    
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public @ResponseBody Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name){
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
 }
