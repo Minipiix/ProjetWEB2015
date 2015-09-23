efc38d90731ef48c0545a324ad54a02bf065f7e9
Alisielle <theshad91@hotmail.fr>
2015-09-23 16:22:35 +0200

    Add DB and create connector to DB

---
+++ src/main/java/esiea/projetweb2015/bddConnector.java
@@ -0,0 +1,22 @@
+package esiea.projetweb2015;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class bddConnector{
+	
+	public static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("org.h2.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	    String DBurl = "jdbc:h2:~/test";
+	    Connection con = DriverManager.getConnection(DBurl,"sa","");
+		return con;
+	    
+	}
+}
