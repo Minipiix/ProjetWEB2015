b789770d4b3f074e388c6ae1066d562ece8ebf69
Alisielle <theshad91@hotmail.fr>
2015-09-23 16:21:17 +0200

    recuperation de la BDD

--- src/main/java/esiea/projetweb2015/HelloWorldConfiguration.java
+++ src/main/java/esiea/projetweb2015/HelloWorldConfiguration.java
@@ -1,5 +1,10 @@
 package esiea.projetweb2015;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +12,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldConfiguration {
     
 	public static void main(String[] args) {
-        SpringApplication.run(HelloWorldConfiguration.class, args);
+        //SpringApplication.run(HelloWorldConfiguration.class, args);
+        
+        ResultSet result = null;
+        String requete = "SELECT * FROM BDD";
+        try {
+        Connection con = bddConnector.getConnection();
+        Statement stmt = con.createStatement();
+        result = stmt.executeQuery(requete);
+        System.out.println(result);
+        } catch (SQLException e) {
+        //traitement de l'exception
+        	e.printStackTrace();
+        }
+        
     }
 
 }
