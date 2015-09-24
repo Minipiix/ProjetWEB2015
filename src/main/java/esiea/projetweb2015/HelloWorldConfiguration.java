8fd58ffc3ef3e3d2bb6ec411e0dc34b58020e43b
Alisielle <theshad91@hotmail.fr>
2015-09-24 09:41:19 +0200

    Modification of hello world to print the data base

--- src/main/java/esiea/projetweb2015/HelloWorldConfiguration.java
+++ src/main/java/esiea/projetweb2015/HelloWorldConfiguration.java
@@ -17,10 +17,15 @@ public class HelloWorldConfiguration {
         ResultSet result = null;
         String requete = "SELECT * FROM BDD";
         try {
-        Connection con = bddConnector.getConnection();
-        Statement stmt = con.createStatement();
-        result = stmt.executeQuery(requete);
-        System.out.println(result);
+	        Connection con = bddConnector.getConnection();
+	        Statement stmt = con.createStatement();
+	        result = stmt.executeQuery(requete);
+	       	while(result.next()) {
+	       		int id  = result.getInt("ID");
+	       		int gameid = result.getInt("GAMEID");
+	       		String link = result.getString("ARTICLELINK");
+	       		System.out.println(id+"\t"+gameid+"\t"+link);
+	       	}
         } catch (SQLException e) {
         //traitement de l'exception
         	e.printStackTrace();
